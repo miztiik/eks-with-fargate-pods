99f29897f3ee4c070f67fa832614bdd23d4a0f85
Mystique <13oct08@quantumfoam.uni.cc>
2021-07-31 18:51:55 +0200

    LGTM

--- stacks/miztiik_global_args.py
+++ stacks/miztiik_global_args.py
@@ -6,5 +6,5 @@ class GlobalArgs():
     OWNER = "MystiqueAutomation"
     REPO_NAME = "eks-with-fargate-pods"
     SOURCE_INFO = f"https://github.com/miztiik/{REPO_NAME}"
-    VERSION = "2020-06-18"
+    VERSION = "2021-06-18"
     MIZTIIK_SUPPORT_EMAIL = ["mystique@example.com", ]
